6a633592aa37578d4678d255196a3573a1bd6a3e
wnvd <waninaveed998@gmail.com>
2025-07-09 11:14:32 +0530

    able to serve image and added different paths and able to send content types

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/assets/logo.png", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Values("Content-Type: text/plain; charset=utf-8")
+		w.Header().Add("charset", "utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Handler: mux,
