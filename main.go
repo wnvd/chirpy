4635eb29e3d3d2eb150cd2dea82771848a84bc58
wnvd <waninaveed998@gmail.com>
2025-07-10 14:48:10 +0530

    fixed headers in handlers, added meteric routes, middleware, typos in comments

--- main.go
+++ main.go
@@ -4,20 +4,54 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricHits(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) resetMetrics() {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	mux := http.NewServeMux()
+	cfg := apiConfig{}
 
 	// home page
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", cfg.middlewareMetricHits(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
-	// serve logo
+	// metrics
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Values("Content-Type: text/plain; charset=utf-8;")
+		w.WriteHeader(http.StatusOK)
+		hitCount := cfg.fileserverHits.Load()
+		response := fmt.Sprintf("Hits: %v", hitCount)
+		w.Write([]byte(response))
+	})
+
+	// reset metrics
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		cfg.resetMetrics()
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8;")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Metrics have been reset!"))
+	})
+
+	// server logo
 	mux.Handle("/app/assets/logo.png", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
-	// health chec
+	// health check
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Values("Content-Type: text/plain; charset=utf-8")
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8;")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
